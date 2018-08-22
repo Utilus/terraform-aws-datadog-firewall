1346fa0855ea8da41994477645263933af0cd288
Miguel Ferreira <miguelferreira@me.com>
2018-08-22 14:57:05 +0200

    Add log statements to ci/test.sh

--- ci/test.sh
+++ ci/test.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env sh
 
-
 cd test
 
 terraform_dir=../.terraform
 if [ -d "${terraform_dir}" ]; then
 
+    echo "==> Re-using terraform directory from previous job"
     cp -rf ../.terraform .
 
 else
 
+    echo "==> Initializing terraform"
     terraform init
 
 fi
 
+echo "==> Applying terraform configuration"
 terraform apply -auto-approve -input=false
 RETURN_VALUE=$?
 
@@ -21,10 +23,13 @@ if [ "${RETURN_VALUE}" -eq 0 ]; then
 
     terraform output --json > verify/files/terraform.json
 
+    echo "==> Running tests"
     inspec exec verify -t aws://eu-west-1/nlo-gateway-dev-env
     RETURN_VALUE=$?
+
 fi
 
+echo "==> Destroying state"
 terraform destroy -auto-approve -input=false
 
 exit ${RETURN_VALUE}
