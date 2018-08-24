d60601a13f432241007e5cae48c4c859b3e547df
Miguel Ferreira <miguelferreira@me.com>
2018-08-24 19:59:44 +0200

    Filter out empty lines from lists of IPs

--- apm.tf
+++ apm.tf
@@ -11,7 +11,7 @@ resource "null_resource" "extract_apm_ip_list" {
   }
 
   provisioner "local-exec" {
-    command = "cat ${local.datadog_ip_ranges_file} | jq -r '.apm .prefixes_ipv4[]' | shuf | head -n ${local.security_group_rule_limit} | tee ${local.apm_ips_file}"
+    command = "cat ${local.datadog_ip_ranges_file} | jq -r '.apm .prefixes_ipv4[] | map(select(length > 0))' | shuf | head -n ${local.security_group_rule_limit} | tee ${local.apm_ips_file}"
   }
 
   depends_on = [
